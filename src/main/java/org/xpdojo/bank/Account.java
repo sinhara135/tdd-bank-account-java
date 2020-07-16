cb76bb597d4fe279e222214bafed36551913e8b7
sinhara135 <rajeevsinha_2003@yahoo.com>
2020-07-16 10:38:33 -0400

    Initial commit to test deposit and withdrawal

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,30 @@
 package org.xpdojo.bank;
 
 public class Account {
+    Double balance;
+
+    public Account(Double initialDeposit) {
+        this.balance = initialDeposit;
+    }
+
+    public Account() {
+        this.balance = 0.0;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void deposit(Double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(Double amount) throws Exception {
+
+        if (balance < amount) {
+            throw new Exception("Not enough balance");
+        } else {
+            balance -= amount;
+        }
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,44 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void openAccountWithInitialBalance() {
+        Account account = new Account(100.0);
+        assertThat(account.getBalance()).isEqualTo(100.0);
     }
+
+    @Test
+    public void openAccountWithNoInitialBalance() {
+        Account account = new Account();
+        assertThat(account.getBalance()).isEqualTo(0.0);
+    }
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(100.0);
+        assertThat(account.getBalance()).isEqualTo(100.0);
+    }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account account = new Account(100.0);
+        account.deposit(50.0);
+        assertThat(account.getBalance()).isEqualTo(150.0);
+    }
+
+    @Test
+    public void notEnoughBalanceToWithdraw() {
+        Account account = new Account(100.0);
+        Exception exception = assertThrows(Exception.class, () -> account.withdraw(120.0));
+        assertTrue(exception.getMessage().contains("Not enough balance"));
+    }
+
 }
